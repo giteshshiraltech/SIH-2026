ee8102af93ecf295d3997dbb7331199c2785db69
giteshshiraltech <giteshshiraltech@gmail.com>
2026-08-08 16:08:52 +0530

    Add files via upload

---
+++ bhoonidhi_fetch.py
@@ -0,0 +1,5 @@
+from config import API_URL, RAW_DATA_FOLDER, PROCESSED_DATA_FOLDER, DEFAULT_CRS
+
+print("Bhoonidhi API:", API_URL)
+print("Raw data folder:", RAW_DATA_FOLDER)
+print("Default CRS:", DEFAULT_CRS)
---
+++ config.py
@@ -0,0 +1,9 @@
+API_URL = "https://bhoonidhi.nrsc.gov.in/stac"
+
+RAW_DATA_FOLDER = "raw_data"
+PROCESSED_DATA_FOLDER = "processed_data"
+LOG_FOLDER = "logs"
+
+DEFAULT_CRS = "EPSG:4326"
+
+API_TIMEOUT = 60
